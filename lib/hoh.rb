8f166db14177c7eaa2785258ed01afbf51340cf5
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-25 13:36:49 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -5,35 +5,33 @@
 # The tests will guide your construction.
 #
 
-#Portion_1 = {
-# :label => "Kingdom",
- # :sub_category => {
-  #  :label => "Phylum",
-   # :sub_category => {
-    #  :label => "Class",
-     # :sub_category => {
-      #  :label => "Order"
-#      }
- #   }
-#  }
-#}
-
- 
- #PORTION_2 = {
-  #:label => "Order"
- #}
- 
- #PORTION_3 = {
-  # :label => "Family",
-   #:sub_category => {
-    #  :label => "Genus",
+#
+# PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
 #   }
 # }
- 
- #PORTION_4 = {
-  # :label => "Species",
-   #:sub_category => nil
-#}
+# 
+# PORTION_2 = {
+#   label: "Order"
+# }
+# 
+# PORTION_3 = {
+#   label: "Family",
+#   sub_category: {
+#     label: "Genus",
+#   }
+# }
+# 
+# PORTION_4 = {
+#   label: "Species",
+#   sub_category: nil
+# }
+
 
   # Remember:
   #  Kingdom
